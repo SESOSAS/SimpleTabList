96ab303931cde57bc82397b53adc4d8c10301e21
SESOSAS <sektria1337@gmail.com>
2022-05-26 16:20:19 +0200

    Fixed a null reference bug

--- src/main/java/de/sesosas/simpletablist/classes/StringFormater.java
+++ src/main/java/de/sesosas/simpletablist/classes/StringFormater.java
@@ -24,26 +24,31 @@ public class StringFormater {
     public static String Get(String text, Player player){
         DecimalFormat df = new DecimalFormat();
         df.setMaximumFractionDigits(2);
-        if(SimpleTabList.getPlugin().config.getBoolean("Chat.Colors")){
-            return text
-                    .replace("&", "§")
-                    .replace(ph("player_name"), Nametag.luckpermsName(player.getPlayer()))
-                    .replace(ph("player_health"), df.format(player.getPlayer().getHealth()))
-                    .replace(ph("player_food"), df.format(player.getPlayer().getFoodLevel()))
-                    .replace(ph("player_ping"), Integer.toString(player.getPlayer().getPing()))
-                    .replace(ph("player_xp"), df.format(player.getPlayer().getExp()))
-                    .replace(ph("player_lvl"), Integer.toString(player.getPlayer().getLevel()))
-                    .replace(ph("player_gamemode"), player.getPlayer().getGameMode().toString());
+        if(text != null){
+            if(SimpleTabList.getPlugin().config.getBoolean("Chat.Colors")){
+                return text
+                        .replace("&", "§")
+                        .replace(ph("player_name"), Nametag.luckpermsName(player.getPlayer()))
+                        .replace(ph("player_health"), df.format(player.getPlayer().getHealth()))
+                        .replace(ph("player_food"), df.format(player.getPlayer().getFoodLevel()))
+                        .replace(ph("player_ping"), Integer.toString(player.getPlayer().getPing()))
+                        .replace(ph("player_xp"), df.format(player.getPlayer().getExp()))
+                        .replace(ph("player_lvl"), Integer.toString(player.getPlayer().getLevel()))
+                        .replace(ph("player_gamemode"), player.getPlayer().getGameMode().toString());
+            }
+            else{
+                return text
+                        .replace(ph("player_name"), Nametag.luckpermsName(player.getPlayer()))
+                        .replace(ph("player_health"), df.format(player.getPlayer().getHealth()))
+                        .replace(ph("player_food"), df.format(player.getPlayer().getFoodLevel()))
+                        .replace(ph("player_ping"), Integer.toString(player.getPlayer().getPing()))
+                        .replace(ph("player_xp"), df.format(player.getPlayer().getExp()))
+                        .replace(ph("player_lvl"), Integer.toString(player.getPlayer().getLevel()))
+                        .replace(ph("player_gamemode"), player.getPlayer().getGameMode().toString());
+            }
         }
         else{
-            return text
-                    .replace(ph("player_name"), Nametag.luckpermsName(player.getPlayer()))
-                    .replace(ph("player_health"), df.format(player.getPlayer().getHealth()))
-                    .replace(ph("player_food"), df.format(player.getPlayer().getFoodLevel()))
-                    .replace(ph("player_ping"), Integer.toString(player.getPlayer().getPing()))
-                    .replace(ph("player_xp"), df.format(player.getPlayer().getExp()))
-                    .replace(ph("player_lvl"), Integer.toString(player.getPlayer().getLevel()))
-                    .replace(ph("player_gamemode"), player.getPlayer().getGameMode().toString());
+            return text;
         }
     }
 }
